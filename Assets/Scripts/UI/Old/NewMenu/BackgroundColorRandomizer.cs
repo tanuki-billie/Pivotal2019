03e95270f71b5c29530dfa82b4b9be2e49f1a43c
snekery <billymyers1020@gmail.com>
2019-07-12 12:59:42 -0700

    Started work on a new menu

--- Assets/Scripts/UI/Old/NewMenu/BackgroundColorRandomizer.cs
+++ Assets/Scripts/UI/Old/NewMenu/BackgroundColorRandomizer.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 namespace ElementStudio.Pivotal.Menu
@@ -6,13 +7,13 @@ namespace ElementStudio.Pivotal.Menu
     public class BackgroundColorRandomizer : MonoBehaviour
     {
         public List<Color> colors = new List<Color>();
-        private Camera cam;
+        private Image cam;
 
         void OnEnable()
         {
-            cam = GetComponent<Camera>();
+            cam = GetComponent<Image>();
             int random = Random.Range(0, colors.Count);
-            cam.backgroundColor = colors[random];
+            cam.color = colors[random];
         }
     }
 }
